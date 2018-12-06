63217c8ac00deb13d4d05c9b7bbfbc3122691123
Luis Montes <luismontes655@gmail.copm>
2018-12-05 22:38:48 -0600

    k

---
+++ views/reporteAgente.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Iniciar Sesión</title>
+      <link rel="stylesheet" href="estilo.css">
+
+  
+</head>
+
+<body>
+holi <?php echo $_GET['id']; ?>
+</body>
+
+</html>
